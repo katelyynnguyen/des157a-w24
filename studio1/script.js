209811fdecd08ba236b5571d4351476a125affbe
Katelyn Nguyen <ktynguyen@ucdavis.edu>
2024-02-08 01:56:48 -0800

    madlibs styling 3

--- studio1/script.js
+++ studio1/script.js
@@ -8,6 +8,7 @@
 
     myForm.addEventListener('submit', function(event){
         event.preventDefault();
+        document.querySelector('#overlay').className = "showing";
         processformData(formData);
     });
     
@@ -33,22 +34,36 @@
 
     function showErrors(formData, emptyfields){
         const errorId = formData[emptyfields[0]].id;
-        const errorText = `please fill out this field ${errorId}`;
+        const errorText = `please enter a ${errorId}`;
         madlibs.innerHTML = errorText;
         document.querySelector(`#${errorId}`).focus();
-    }
 
-
-
+        document.addEventListener('keydown', function(event){
+            if (event.key === 'Escape'){
+                document.getElementById('overlay').className="hidden";
+            }
+        });
+    };
 
     function makeMadlib(words){
-        const myText = `I’ve seen a lot of Instagram reels lately of matcha <span>${words[0]}</span>, so I’ve been very <span>${words[1]}</span>. I looked through my <span>${words[2]}</span> and took our any <span>${words[3]}</span> I may need. I started mixing my <span>${words[4]}</span> ingredients and discovered I ran out of <span>${5}</span> so I used <span>${words[6]}</span> instead. The batter looked really <span>${words[7]}</span>, but maybe that’s because I used <span>${words[8]}</span> eggs. I decided to bake it for <span>${words[9]}</span> minutes at <span>${words[10]}</span> degrees. When it’s done I think I’ll let my <span>${words[11]}</span> try it first.`;
+        const myText = `I’ve seen a lot of Instagram reels lately of matcha <span>${words[0]}</span>, so I’ve been very <span>${words[1]}</span>. I looked through my <span>${words[2]}</span> and took out any <span>${words[3]}</span> I may need. I started mixing my <span>${words[4]}</span> ingredients and discovered I ran out of <span>${words[5]}</span> so I used <span>${words[6]}</span> instead. The batter looked really <span>${words[7]}</span>, but maybe that’s because I used <span>${words[8]}</span> eggs. I decided to bake it for <span>${words[9]}</span> minutes at <span>${words[10]}</span> degrees. When it’s done I think I’ll let my <span>${words[11]}</span> try it first.`;
 
-        madlibs.innerHTML = myText; 
+        madlibs.innerHTML += myText;
         
         for (const eachField of formData){
             eachField.value = '';
         }
+
+        document.querySelector('.close').addEventListener('click', function (event){
+            event.preventDefault();
+            document.querySelector('#overlay').className = "hidden";
+        });
+    
+        document.addEventListener('keydown', function(event){
+            if (event.key === 'Escape'){
+                document.getElementById('overlay').className="hidden";
+            }
+        })
     }
 
 } )();
