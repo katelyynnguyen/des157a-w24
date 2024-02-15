1c0b05f6db9913ddd04d1fe3e30f8bba1b71883d
Katelyn Nguyen <ktynguyen@ucdavis.edu>
2024-02-14 22:52:17 -0800

    learning journal 4

--- studio1/script.js
+++ studio1/script.js
@@ -38,6 +38,11 @@
         madlibs.innerHTML = errorText;
         document.querySelector(`#${errorId}`).focus();
 
+        document.querySelector('.close').addEventListener('click', function (event){
+            event.preventDefault();
+            document.querySelector('#overlay').className = "hidden";
+        });
+
         document.addEventListener('keydown', function(event){
             if (event.key === 'Escape'){
                 document.getElementById('overlay').className="hidden";
