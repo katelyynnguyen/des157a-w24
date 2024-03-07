e1ac8cf286cbdca1205706f4298e1e3f5374852a
Katelyn Nguyen <ktynguyen@ucdavis.edu>
2024-03-06 23:34:24 -0800

    project brief

---
+++ studio3/script.js
@@ -0,0 +1,10 @@
+(function (){
+    'use script';
+    console.log("reading js");
+
+    const startGame = document.querySelector('#startgame');
+    const gameControl = document.querySelector('#gamecontrol');
+    const game = document.querySelector('#game');
+    const actionArea = document.querySelector('#action');
+    const score = document.querySelector('#score');
+})();
